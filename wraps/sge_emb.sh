2f2fa2a7947c203f901ff6141496e1700624afab
Ronald Acosta (student <acosta@sol1.ufal.hide.ms.mff.cuni.cz>
2019-04-23 19:29:37 +0200

    local

--- wraps/sge_emb.sh
+++ wraps/sge_emb.sh
@@ -2,7 +2,7 @@
 
 treebank=$1
 
-qsub -pe smp 50 -cwd -l mem_free=40G,act_mem_free=40G,h_data=60G -p -50 \
+qsub -pe smp 20 -cwd -l mem_free=40G,act_mem_free=40G,h_data=60G -p -50 \
 -o emb/logs/$treebank.out \
 -e emb/logs/$treebank.err \
 wraps/train_emb_ft.sh $treebank
