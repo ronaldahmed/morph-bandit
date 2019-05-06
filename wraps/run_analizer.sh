0bebb1c99c9cd51a255a54382033099a97fd0228
Ronald <roncardenasacosta@gmail.com>
2019-05-07 00:40:54 +0200

    all dist task2

--- wraps/run_analizer.sh
+++ wraps/run_analizer.sh
@@ -7,11 +7,11 @@ python3 run_analizer.py --mode train \
 --train_file data/$tbname/train \
 --dev_file data/$tbname/dev \
 --epochs 20 \
---batch_size 32 \
+--batch_size 128 \
 --mlp_size 100 \
---emb_size 300 \
---learning_rate 0.0005 \
---dropout 0.1 \
+--emb_size 140 \
+--learning_rate 0.00069 \
+--dropout 0.19 \
 --model_save_dir $mdir \
 --scheduler \
 --gpu
