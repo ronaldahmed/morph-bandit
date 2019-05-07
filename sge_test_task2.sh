c18388d871635819a35a034837edadb629ce42ff
Ronald <roncardenasacosta@gmail.com>
2019-05-07 10:48:57 +0200

    cov test eval

--- sge_test_task2.sh
+++ sge_test_task2.sh
@@ -6,9 +6,13 @@ batch=$1
 for tb in $(cut -f 2 -d " " $batch); do
 	echo $tb
 	# bash wraps/run_analizer.sh $tb models-segm/$tb
-	op_ep=$(tail -1 models-segm/$tb/log.out | cut -f 2)
+	op_ep=$(tail -1 models-segm/$tb/log.out | cut -f 1)
+
+	if [ $op_ep == "0" ]; then
+		op_ep="19"
+	fi
 	input_model=models-segm/$tb/segm_$op_ep.pth
-	echo "\t$input_model"
+	echo "$input_model"
 
 	# qsub -q 'gpu*' -cwd -l gpu=1,gpu_cc_min3.5=1,gpu_ram=4G,mem_free=10G,act_mem_free=10G,h_data=15G -p -10 \
 	# -o models-segm/$tb/log-test.out \
