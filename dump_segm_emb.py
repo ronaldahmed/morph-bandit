c5301a32170d00b9c7872485f4b274b134edb1ee
Ronald <roncardenasacosta@gmail.com>
2019-05-07 15:03:09 +0200

    cov test eval

--- dump_segm_emb.py
+++ dump_segm_emb.py
@@ -8,25 +8,29 @@ from trainer import Trainer
 
 
 if __name__ == '__main__':
-  args = analizer_args()
+  # args = analizer_args()
 
   tbnames = open("tbnames_order_tab.txt",'r').read().strip("\n").split("\n")
 
   for tb in tbnames:
+  	print(tb)
+  	
   	log_fn = "models-segm/"+tb+"/log.out"
   	ep_acc_line = open(log_fn,'r').read().strip("\n").split("\n")[-1]
   	ep,_ = ep_acc_line.split("\t")
   	input_model = "models-segm/%s/segm_%s.pth" % (tb,ep)
 
-  	tb_args = args
-  	tb_args.input_model = input_model
-  	tb_args.train_file = "data/"+tb+"/train"
+  	# tb_args = args
+  	# tb_args.input_model = input_model
+  	# tb_args.train_file = "data/"+tb+"/train"
+  	# loader = DataLoaderAnalizer(tb_args)
 
-  	loader = DataLoaderAnalizer(tb_args)
-  	state_dict = torch.load(tb_args.input_model)
+  	state_dict = torch.load(input_model)
   	emb_matrix = state_dict["emb.weight"]
-  	outname = 
-  	torch.save()
+  	outname = "models-segm/%s/ops.bin" % (tb)
+  	torch.save(emb_matrix,outname)
+
+  	break
 
 
 
