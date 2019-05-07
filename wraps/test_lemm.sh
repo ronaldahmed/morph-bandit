9fa29fc9ffc4f895fa3a161b89e99943e6cd64a0
Ronald <roncardenasacosta@gmail.com>
2019-05-07 11:39:05 +0200

    cov test eval

--- wraps/test_lemm.sh
+++ wraps/test_lemm.sh
@@ -7,16 +7,33 @@ imodel=$3
 cd /home/acosta/morph-bandit/
 conda activate morph
 
-python3 run_analizer.py --mode $mode \
---train_file data/$tbname/train \
---test_file data/$tbname/test \
---epochs 20 \
---batch_size 128 \
---mlp_size 100 \
---emb_size 140 \
---learning_rate 0.00069 \
---dropout 0.19 \
---scheduler \
---input_model $imodel \
---gpu
+if [ $mode == "covered-test" ]||[ $mode == "test" ]; then
+	python3 run_analizer.py --mode $mode \
+	--train_file data/$tbname/train \
+	--test_file data/$tbname/test \
+	--epochs 20 \
+	--batch_size 128 \
+	--mlp_size 100 \
+	--emb_size 140 \
+	--learning_rate 0.00069 \
+	--dropout 0.19 \
+	--scheduler \
+	--input_model $imodel \
+	--gpu
 
+elif [ $mode == "dev" ]; then
+	python3 run_analizer.py --mode $mode \
+	--train_file data/$tbname/train \
+	--dev_file data/$tbname/dev \
+	--epochs 20 \
+	--batch_size 128 \
+	--mlp_size 100 \
+	--emb_size 140 \
+	--learning_rate 0.00069 \
+	--dropout 0.19 \
+	--scheduler \
+	--input_model $imodel \
+	--gpu
+
+
+fi
