2587b2f55fb56c44b1b3560105a604ee39e72b9a
Ronald <roncardenasacosta@gmail.com>
2019-05-07 13:59:32 +0200

    cov test eval

---
+++ order_tabres.py
@@ -0,0 +1,11 @@
+order_ = open("tbnames_order_tab.txt",'r').read().split("\n")
+to_map = {}
+for line in open("models/res.dev.csv"):
+	line = line.strip("\n")
+	if line=="": continue
+	tb,acc,edist = line.split(",")
+	to_map[tb] = (acc,edist)
+
+outfile = open("ordered_res.dev.csv",'w')
+for tb in order_:
+	print("%s,%s,%s" % (tb,to_map[tb][0],to_map[tb][1]), file = outfile )
