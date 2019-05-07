f26b69a115e3548cfc3ca349fbca556d20956091
Ronald <roncardenasacosta@gmail.com>
2019-05-07 21:17:29 +0200

    cov test eval

--- debug_sge_sgm.sh
+++ debug_sge_sgm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 batch=$1
+mode=$2
 for tb in $(cut -f 2 -d " " $batch); do
 	echo "::$tb ----------------------------"
-	cat models-segm/$tb/log.err
+	cat models-segm/$tb/log-$mode.err
 done
