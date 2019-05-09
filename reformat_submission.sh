db13f116e54310a6bca96cc0e86360354a1b244a
Ronald <roncardenasacosta@gmail.com>
2019-05-09 18:04:01 +0200

    cov test eval

--- reformat_submission.sh
+++ reformat_submission.sh
@@ -3,11 +3,11 @@
 dirname="CHARLES-MALTA-01-2"
 mkdir -p $dirname
 
-ud_tb_names="data/uddir_tbname"
+ud_tb_names=data/uddir_tbname
 
-for line in $(cat "$ub_tb_names"); do
-	uddir="${ cut -f 1 -d " " line }"
-	tbname="${ cut -f 2 -d " " line }"
+for line in $(cat "$ud_tb_names"); do
+	uddir=$(cut -f 1 -d " " $line)
+	tbname=$(cut -f 2 -d " " $line)
 	mkdir -p $dirname/$uddir
 	cp data/$tbname/test.anlz.conllu.pred $dirname/$uddir/$tbname-um-test.output
 done
