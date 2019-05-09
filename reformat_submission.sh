ce02070dec67e025d09add701079ccce39f8f85c
Ronald <roncardenasacosta@gmail.com>
2019-05-09 18:09:44 +0200

    cov test eval

--- reformat_submission.sh
+++ reformat_submission.sh
@@ -5,9 +5,10 @@ mkdir -p $dirname
 
 ud_tb_names=data/uddir_tbname
 
-for line in $(cat "$ud_tb_names"); do
-	uddir=$(cut -f 1 -d " " $line)
-	tbname=$(cut -f 2 -d " " $line)
+#for line in $(cat "$ud_tb_names"); do
+while read line; do
+	uddir=$(echo $line | cut -f 1 -d " ")
+	tbname=$(echo $line | cut -f 2 -d " ")
 	mkdir -p $dirname/$uddir
 	cp data/$tbname/test.anlz.conllu.pred $dirname/$uddir/$tbname-um-test.output
-done
+done < $ud_tb_names
