1342e18d5199198baa75a497781c2dca0ed5d9cf
Ronald <roncardenasacosta@gmail.com>
2019-05-09 09:00:05 +0200

    dev feats

--- sge_test_task2.sh
+++ sge_test_task2.sh
@@ -16,10 +16,13 @@ for tb in $(cut -f 2 -d " " $batch); do
 	fi
 
 	input_model=models-segm/$tb/segm_$op_ep.pth
+	emb_file=models-segm/$tb/emb.pth
 	
 	qsub -q 'gpu*' -cwd -l gpu=1,gpu_cc_min3.5=1,gpu_ram=4G,mem_free=10G,act_mem_free=10G,h_data=15G -p -10 \
 	-o models-segm/$tb/log-$mode.out \
 	-e models-segm/$tb/log-$mode.err \
-	wraps/test_lemm.sh $tb $mode $input_model
+	wraps/test_anlz.sh $tb $mode $input_model $emb_file
+	# wraps/test_lemm.sh $tb $mode $input_model
+
 
 done
