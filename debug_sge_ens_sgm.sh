0a36443caa14ca24ee9dc5908f99bb4e263405d4
Ronald <roncardenasacosta@gmail.com>
2019-05-25 19:36:39 +0200

    howdy

--- debug_sge_ens_sgm.sh
+++ debug_sge_ens_sgm.sh
@@ -7,7 +7,7 @@ seeds="5167 4783 2311 9173 4793 1019 8431 673 2531 2953"
 for tb in $(cut -f 2 -d " " $batch); do
 	echo "::$tb ----------------------------"
 	for seed in $seeds; do
-		cat models-ens/$tb/log-$seed.err
+		cat models-ens/$tb/lem-$seed-log.err
 		echo ""
 	done
 	echo ""
