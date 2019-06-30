d3cfbc6a7e950eaffe070038cf95fe319c83b7f5
Ronald <roncardenasacosta@gmail.com>
2019-06-30 12:48:13 +0100

    endless debugging

--- sge_rnd_search_anlz.sh
+++ sge_rnd_search_anlz.sh
@@ -10,10 +10,10 @@ op_ep=$(tail -1 models-segm/$tb/log.out | cut -f 1)
 input_model=models-segm/$tb/segm_$op_ep.pth
 emb_file=models-segm/$tb/emb.pth
 
-# qsub -q 'gpu*' -cwd -l gpu=1,gpu_cc_min3.5=1,gpu_ram=3G,mem_free=10G,act_mem_free=10G,h_data=15G -p -5 \
-# -o models-anlz/$tb/log-rnd-search.out \
-# -e models-anlz/$tb/log-rnd-search.err \
-# wraps/random_search_anlz.sh $tb $input_model $emb_file $beam_size 42
+qsub -q 'gpu*' -cwd -l gpu=1,gpu_cc_min3.5=1,gpu_ram=3G,mem_free=10G,act_mem_free=10G,h_data=15G -p -5 \
+-o models-anlz/$tb/log-rnd-search.out \
+-e models-anlz/$tb/log-rnd-search.err \
+wraps/random_search_anlz.sh $tb $input_model $emb_file $beam_size 42
 
-bash wraps/random_search_anlz.sh $tb $input_model $emb_file $beam_size 42
+# bash wraps/random_search_anlz.sh $tb $input_model $emb_file $beam_size 42
 
