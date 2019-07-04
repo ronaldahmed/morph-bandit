5c4d898ca64ba3e9a8ef1b7c12150c27317713ff
Ronald <roncardenasacosta@gmail.com>
2019-07-04 23:37:00 +0100

    local

--- dump_e-lem_vec.py
+++ dump_e-lem_vec.py
@@ -12,29 +12,24 @@ if __name__ == '__main__':
   tbnames = ["es_ancora"]
   
   for tb in tbnames:
-  	print(tb)
+    print(tb)
 
-  	log_fn = "models-segm/"+tb+"/log.out"
-  	ep_acc_line = open(log_fn,'r').read().strip("\n").split("\n")[-1]
-  	ep,_ = ep_acc_line.split("\t")
-  	input_model = "models-segm/%s/segm_%s.pth" % (tb,ep)
+    log_fn = "models-segm/"+tb+"/log.out"
+    ep_acc_line = open(log_fn,'r').read().strip("\n").split("\n")[-1]
+    ep,_ = ep_acc_line.split("\t")
+    input_model = "models-segm/%s/segm_%s.pth" % (tb,ep)
 
-  	tb_args = args
-  	tb_args.input_model = input_model
-  	tb_args.train_file = "data/"+tb+"/train"
+    tb_args = args
+    tb_args.input_model = input_model
+    tb_args.train_file = "data/"+tb+"/train"
     tb_args.dev_file = "data/"+tb+"/dev"
-  	loader = DataLoaderAnalizer(tb_args)
+    loader = DataLoaderAnalizer(tb_args)
 
-  	state_dict = torch.load(input_model,map_location='cpu')
-  	emb_matrix = state_dict["emb.weight"]
-  	
+    state_dict = torch.load(input_model,map_location='cpu')
+    emb_matrix = state_dict["emb.weight"]
+    
     pdb.set_trace()
 
     print("-->")
 
 
-
-
-
-  
-  
--- wraps/run_dump_emb.sh
+++ wraps/run_dump_emb.sh
@@ -7,9 +7,4 @@ conda activate morph
 
 python3 dump_e-lem_vec.py --mode dev \
 --train_file data/$tbname/train \
---dev_file data/$tbname/dev \
---embedding_pth foo \
---input_lem_model foo \
---src_ref "anlz" \
---tgt_ref "anlz.fine-seq"
-
+--dev_file data/$tbname/dev
