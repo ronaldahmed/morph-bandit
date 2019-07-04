f75c090a6f205932feec605adc2de15bbc222bbc
Ronald <roncardenasacosta@gmail.com>
2019-07-04 23:35:51 +0100

    local

--- wraps/run_dump_emb.sh
+++ wraps/run_dump_emb.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-tbname=$1
+tbname=foo
 
 cd /home/acosta/morph-bandit/
 conda activate morph
 
-python3 error_analysis.py --mode dev \
+python3 dump_e-lem_vec.py --mode dev \
 --train_file data/$tbname/train \
 --dev_file data/$tbname/dev \
 --embedding_pth foo \
 --input_lem_model foo \
 --src_ref "anlz" \
 --tgt_ref "anlz.fine-seq"
+
