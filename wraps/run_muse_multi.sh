38b0dbe14b277fd9495c42b827b8b91f7fbdcdc9
Ronald <roncardenasacosta@gmail.com>
2019-07-07 13:14:47 +0100

    proj xx-es

--- wraps/run_muse_multi.sh
+++ wraps/run_muse_multi.sh
@@ -15,6 +15,7 @@ tgt_emb="$HOME/morph-bandit/l1-mono-emb/es_ancora.vec"
 python unsupervised.py --src_lang $lsrc --tgt_lang es \
 --src_emb $src_emb --tgt_emb $tgt_emb \
 --emb_dim 140 \
+--dis_most_frequent 10000 \
 --n_refinement 5 \
 --normalize_embeddings center \
 --seed 42 \
