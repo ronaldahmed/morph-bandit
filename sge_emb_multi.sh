3703a2f37c4496baa97d5151194c7f910696c2c8
Ronald <roncardenasacosta@gmail.com>
2019-07-07 14:29:20 +0100

    proj xx-es

--- sge_emb_multi.sh
+++ sge_emb_multi.sh
@@ -5,7 +5,7 @@ src=$2
 
 basefn="$HOME/morph-bandit/l1-multi-emb/log_$lsrc-es"
 
-qsub -q 'gpu*' -cwd -l gpu=1,gpu_cc_min3.5=1,gpu_ram=4G,mem_free=10G,act_mem_free=10G,h_data=15G -p -10 \
+qsub -q 'gpu*' -cwd -l gpu=1,gpu_cc_min3.5=1,gpu_ram=4G,mem_free=8G,act_mem_free=8G,h_data=10G -p -10 \
 -o $basefn.out \
 -e $basefn.err \
 wraps/run_muse_multi.sh $lsrc $src
