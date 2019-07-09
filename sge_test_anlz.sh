0bb72f208723dda2bd4bb976e8b28bc20fda4fbb
ronacost <ronacost@ud360e8120ef852.ant.amazon.com>
2019-07-09 21:55:17 +0100

    mtdnn adapted

--- sge_test_anlz.sh
+++ sge_test_anlz.sh
@@ -18,15 +18,33 @@ mkdir -p models_pred
 
 for tb in $(cut -f 2 -d " " $batch); do
 	echo $tb
-	
+	lang_name=${tb:0:2}
 	op_ep_seg=$(tail -1 models-segm/$tb/log.out | cut -f 1)
-	# op_ep_anl=$(tail -1 models-anlz/$tb/log.out | cut -f 1)
-	op_ep_anl=$(tail -1 models-anlz/$tb/log-$exp.out | cut -f 1)
-
 	input_lem_model=models-segm/$tb/segm_"$op_ep_seg".pth
-	# input_anlz_model=models-anlz/$tb/anlz_"$op_ep_anl".pth
-	input_anlz_model=models-anlz/$tb/$exp_"$op_ep_anl".pth
-	emb_file=models-segm/$tb/emb.pth
+	op_ep_anl=""
+	input_anlz_model=""
+	emb_file=""
+
+	if [ $exp == "l1-a1" ]; then
+		op_ep_anl=$(tail -1 models-anlz/$tb/log.out | cut -f 1)
+		input_anlz_model=models-anlz/$tb/anlz_"$op_ep_anl".pth
+	elif [ $exp == "l1-a2" ]; then
+		op_ep_anl=$(tail -1 models-anlz/$tb/log-l1a2.out | cut -f 1)
+		input_anlz_model=models-anlz/$tb/anlz_fine-seq_"$op_ep_anl".pth
+	else
+		op_ep_anl=$(tail -1 models-anlz/$tb/log-$exp.out | cut -f 1)
+		input_anlz_model=models-anlz/$tb/$exp_"$op_ep_anl".pth
+	fi
+
+	if [ ${exp:0:2} == "l1" ]; then
+		emb_file=models-segm/$tb/emb.pth
+	elif [ ${exp:0:2} == "ml1" ]; then
+		if [ $lang_name == "es" ]; then
+			emb_file=l1-multi-emb/cs-es/cs-es/vectors-es.pth
+		else
+			emb_file=l1-multi-emb/"$lang_name"-es/"$lang_name"-es/vectors-"$lang_name".pth
+		fi
+	fi
 	
 	qsub -q 'gpu-troja.q' -cwd -l gpu=1,gpu_cc_min3.5=1,gpu_ram=4G,mem_free=10G,act_mem_free=10G,h_data=15G -p -10 \
 	-o models-anlz/$tb/log-$exp-$mode.out \
