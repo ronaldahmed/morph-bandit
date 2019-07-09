8bba8b7183aa9376a0d21590cdc5dd00b7b4d473
ronacost <ronacost@ud360e8120ef852.ant.amazon.com>
2019-07-09 21:20:05 +0100

    mtdnn adapted

--- eval_grained_f1_conllu.py
+++ eval_grained_f1_conllu.py
@@ -17,6 +17,7 @@ feats_vocab = uploadObject(feats_vocab_fn)
 gold_feats = []
 pred_feats = []
 acc = 0.0
+acc_bundle = 0.0
 
 for gline,pline in zip(glines,plines):
 	gline = gline.strip("\n")
@@ -30,9 +31,12 @@ for gline,pline in zip(glines,plines):
 	gfeats.sort()
 	pfeats.sort()
 	acc += int(";".join(gfeats)==";".join(pfeats))
+	acc_bundle += int(gcols[5]==pcols[5])
 	gold_feats.append([feats_vocab.get_label_id(x) for x in gfeats])
 	pred_feats.append([feats_vocab.get_label_id(x) for x in pfeats])
 	
+	if ";".join(gfeats) != gcols[5]:
+		print("%20s %20s | %20s %20s" % (gcols[5],pcols[5],";".join(gfeats),";".join(pfeats)) )
 	# pdb.set_trace()
 #
 
@@ -46,6 +50,7 @@ for i in range(nw):
 
 
 msd_acc = (100.0*acc) / nw
+msd_acc_bundle = (100.0*acc_bundle) / nw
 f1 = 100.0*f1_score(gmat,pmat,average="micro") # average is pessimistic
 
-print("%.2f %.2f" % (msd_acc,f1))
+print("%.2f %.2f %.2f" % (msd_acc_bundle,msd_acc,f1))
--- sge_test_anlz.sh
+++ sge_test_anlz.sh
@@ -5,7 +5,7 @@ exp=$2
 
 bsearch=-1
 tagger_mode="bundle"
-eval_mode="fine"
+eval_mode="bundle"
 mode=dev  # dev, test, covered-test
 
 if [ ${exp:-2} == "a1" ]; then
@@ -19,12 +19,12 @@ for tb in $(cut -f 2 -d " " $batch); do
 	echo $tb
 	# bash wraps/run_analizer.sh $tb models-segm/$tb
 	op_ep_seg=$(tail -1 models-segm/$tb/log.out | cut -f 1)
-	op_ep_anl=$(tail -1 models-anlz/$tb/log.out | cut -f 1)
-	# op_ep_anl=$(tail -1 models-anlz/$tb/log-$exp.out | cut -f 1)
+	# op_ep_anl=$(tail -1 models-anlz/$tb/log.out | cut -f 1)
+	op_ep_anl=$(tail -1 models-anlz/$tb/log-$exp.out | cut -f 1)
 
 	input_lem_model=models-segm/$tb/segm_"$op_ep_seg".pth
-	input_anlz_model=models-anlz/$tb/anlz_"$op_ep_anl".pth
-	# input_anlz_model=models-anlz/$tb/anlz_$exp_"$op_ep_anl".pth
+	# input_anlz_model=models-anlz/$tb/anlz_"$op_ep_anl".pth
+	input_anlz_model=models-anlz/$tb/$exp_"$op_ep_anl".pth
 	emb_file=models-segm/$tb/emb.pth
 	
 	qsub -q 'gpu-troja.q' -cwd -l gpu=1,gpu_cc_min3.5=1,gpu_ram=4G,mem_free=10G,act_mem_free=10G,h_data=15G -p -10 \
