97ee491f74231c17bd0bee720e20f494e611b061
ronacost <ronacost@ud360e8120ef852.ant.amazon.com>
2019-07-09 22:17:12 +0100

    mtdnn adapted

--- debug_sge_anlz.sh
+++ debug_sge_anlz.sh
@@ -2,10 +2,12 @@
 
 batch=$1
 mode=$2
+exp=$3
+
 for tb in $(cut -f 2 -d " " $batch); do
 	echo "::$tb ----------------------------"
 	# cat models-anlz/$tb/log.err
-	cat models-anlz/$tb/log-l1a2.err
+	cat models-anlz/$tb/log-$exp-$mode.err
 	echo ""
 	echo ""
 done
