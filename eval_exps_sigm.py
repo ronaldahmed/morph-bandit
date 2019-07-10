f7715b55052380d0fafc4937ca239c241300b677
ronacost <ronacost@ud360e8120ef852.ant.amazon.com>
2019-07-10 13:50:17 +0100

    b0

---
+++ eval_exps_sigm.py
@@ -0,0 +1,39 @@
+import sys,os
+import numpy as np
+from evaluate_2019_task2 import *
+from pathlib import Path
+
+exps = [
+  "l1-a1",
+  "l1-a2",
+  "ml1-a1-bundle",
+]
+suff = "all"
+
+if len(sys.argv)<2:
+  print("usage: python eval_exps_sigm.py <mode> [<exp_id>]")
+  sys.exit(1)
+
+mode=sys.argv[1]
+if len(sys.argv)>2:
+  exps = [sys.argv[2]]
+  suff = sys.argv[2]
+
+tbnames = [x.strip('\n').split(" ") for x in open("data/tbnames-thesis","r") if x.strip("\n")!=""]
+outfile = "experiments/%s-metrics.thesis" % suff
+
+with open(outfile,'w') as outfile:
+  print("tbname,exp,lem-acc,edist,m-acc,m-f1",file=outfile)
+  for uddir,tb in tbnames:
+    print(tb)
+    gold_fn = "2019/task2/%s/%s-um-%s.conllu" % (uddir,tb,mode)
+    reference = read_conllu(Path(gold_fn))
+
+    for exp in exps:
+      pred_fn = "models_pred/%s-um-%s.conllu.%s.pred" % (tb,mode,exp)
+      output = read_conllu(Path(pred_fn))
+      results = manipulate_data(input_pairs(reference, output))
+      print(tb,exp,*["{0:.2f}".format(v) for v in results],sep=",",file=outfile)
+
+
+
