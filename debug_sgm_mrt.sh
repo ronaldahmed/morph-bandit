a723f3e02544a0ccceafa61ac43064c29765da9b
Ronald <roncardenasacosta@gmail.com>
2019-07-12 09:03:23 +0100

    arg parsers bash mrt

--- debug_sgm_mrt.sh
+++ debug_sgm_mrt.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 tbname="es_ancora"
+optmzs="adam adadelta"
+alphas="0.01 0.001 0.0001"
 
-
-for optm in "adam adadelta"; do
-	for alpha in "0.01 0.001 0.0001"; do
-		fn=models-segm/$tbname/l1.mrt_optm-"$optm"_alpha-$alpha_sample-10/
+for optm in $optmzs; do
+	for alpha in $alphas; do
+		fn=models-segm/$tbname/l1.mrt_optm-"$optm"_alpha-"$alpha"_sample-10/
 		echo ":: optm=$optm  alpha=$alpha  sample=10"
 		tail -40 $fn/log.out
 		echo ""
