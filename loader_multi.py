ce52b062990821096c872f4a46c09a7cd41eda58
ronacost <ronacost@ud360e8120ef852.ant.amazon.com>
2019-07-12 18:25:47 +0100

    mtr debugging

--- loader_multi.py
+++ loader_multi.py
@@ -6,7 +6,9 @@ import json
 
 TREEBANK_TEMPLATE = "2019/task2/%s/%s-um-%s.conllu"
 ACTION_FILE_TEMPLATE = "data/%s/%s"
+TOKENIZED_JSON_TEMPLATE = "data/%s/%s-%s.json"  # tbset, tb, split
 MULTI_LING_CONF_JSON = "multi_ling_conf.json"
+
 
 def load_set_conf(tbset):
   conf = json.load(open(MULTI_LING_CONF_JSON,'r'))
@@ -14,12 +16,12 @@ def load_set_conf(tbset):
   return conf[tbset]
 
 def loader_from_pickle(args):
-  filename = "data/%s-%s.loader.pickle" % (args.src_set,args.tgt)
+  filename = "data/%s-%s/loader.pickle" % (args.src_set,args.tgt)
   if os.path.exists(filename):
     return uploadObject(filename)
   else:
     return DataLoaderMLing(args)
-    
+
 
 class DataLoaderMLing(object):
   def __init__(self,args):
@@ -84,12 +86,29 @@ class DataLoaderMLing(object):
       sample["bundle_feat"].append(self.vocab_feat_bundles.add(feats))
       sample["fine_feat"].append([sos] + [self.vocab_feats.add(feat) for feat in feats.split(";")] + [eos])
       #
-
     self.vocabs_input_mode_toggle(False)
+    return data
+
+
+  def dump_tokenized(self,data,filename):
+    with open(filename,'w') as outfile:
+      for sample in data:
+        outfile.write(json.dumps(sample))
 
 
   def load_vocab(self):
+    def read_dump_tok(tb_list,fill_train):
+      for tb in tb_list:
+        train_tb = self.read_action_file(tb,"train",fill_vocab=fill_train)
+        dev_tb = self.read_action_file(tb,"dev",fill_vocab=False)
+        test_tb = self.read_action_file(tb,"test",fill_vocab=False)
+        for data,split in zip([train_tb,dev_tb,test_tb],["train","dev","test"]):
+          self.dump_tokenized(train_tb,TOKENIZED_JSON_TEMPLATE % (tb,split))
 
+
+    train_tb = self.read_action_file(tb,"train",fill_vocab=True)
+    dev_tb = self.read_action_file(tb,"dev",fill_vocab=False)
+    test_tb = self.read_action_file(tb,"test",fill_vocab=False)
 
   def load_data(self,fill_vocab=False):
 
