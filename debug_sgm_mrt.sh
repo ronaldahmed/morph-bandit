bf87be3f33bbeadeebc734adccb94fec01b16e86
Ronald <roncardenasacosta@gmail.com>
2019-07-13 14:21:33 +0100

    mrt tweaking

--- debug_sgm_mrt.sh
+++ debug_sgm_mrt.sh
@@ -12,7 +12,7 @@ for optm in $optmzs; do
 		for clip in $clips; do
 			fn=models-segm/$tbname/l1.mrt.warm_optm-"$optm"_alpha-"$alpha"_sample-10_clip-"$clip"/
 			echo ":: optm=$optm  alpha=$alpha  sample=10  clip=$clip"
-			tail -40 $fn/log.out
+			tail -40 $fn/log.err
 			echo ""
 			echo ""
 		done
