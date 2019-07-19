f91aefd4245007a8d3166662360f9d1038132ff1
ronacost <ronacost@ud360e8120ef852.ant.amazon.com>
2019-07-19 13:05:59 +0100

    b2 run 100

---
+++ cipher/fix_pos.py
@@ -0,0 +1,19 @@
+import sys
+from utils import um2char
+import re
+
+infn = sys.argv[1]
+outfn = sys.argv[2]
+
+re_pats = []
+for umpos,ch in um2char.items():
+	tmp = "".join(["["+x+"]" for x in umpos])
+	pat = re.compile(r"\s" + tmp + r"\s",re.UNICODE)
+	re_pats.append([pat,ch])
+
+outfile = open(outfn,'w')
+for line in open(infn,'r'):
+	for pat,ch in re_pats:
+		line = pat.subs(ch,line)
+	print(line,file=outfile)
+
