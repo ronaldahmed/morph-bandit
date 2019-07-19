01d8598dd264f419e6c1b73c8d45b2accca4c35c
Ronald <roncardenasacosta@gmail.com>
2019-07-19 02:58:48 +0100

    cipher exps

--- cipher/sge_exp.sh
+++ cipher/sge_exp.sh
@@ -10,8 +10,9 @@ niters=500
 order=2
 
 cd $HOME/morph-bandit/cipher
+log_dir=$HOME/universal-lang-tools-playground
 
 qsub -cwd -l mem_free=15G,act_mem_free=15G,h_vmem=20G -p -10 \
--o logs/$rl$order-"$il"."$base".$nc.$niters.pipeline \
--e logs/$rl$order-"$il"."$base".$nc.$niters.err \
+-o $log_dir/$rl$order-"$il"."$base".$nc.$niters.pipeline \
+-e $log_dir/$rl$order-"$il"."$base".$nc.$niters.err \
 run_exp.sh $rl $il $base $nc $nj
