1b23ee21d68d98e5d06af2ae02f72f7333616348
Ronald <roncardenasacosta@gmail.com>
2019-07-23 08:28:44 +0100

    mrt plots

--- debug_lem_mrt.sh
+++ debug_lem_mrt.sh
@@ -6,8 +6,8 @@ exp="l1.mrt.warm_optm-adadelta_alpha-0.0001_sample-20_clip-0_bs-5_temp-1"
 
 for tb in $(cut -f 2 -d " " $batch); do
 	echo "::$tb ----------------------------"
-	# cat models-segm/$tb/$exp/log-$mode.err
-	cat models-segm/$tb/$exp/log.err
+	cat models-segm/$tb/$exp/log-$mode.err
+	# cat models-segm/$tb/$exp/log.err
 	echo ""
 	echo ""
 done
