e59ec299a501b7d2cb843ce8ab20e9a74518fbf0
Ronald <roncardenasacosta@gmail.com>
2019-07-23 23:11:15 +0100

    migrate files

--- copy_files_thesis.sh
+++ copy_files_thesis.sh
@@ -46,14 +46,17 @@ for tb in $(cut -f 2 -d " " $batch); do
     if [ $exp == "l1-a1" ]; then
         op_ep_anl=$(tail -1 models-anlz/$tb/log.out | cut -f 1)
         input_anlz_model=models-anlz/$tb/anlz_"$op_ep_anl".pth
+        cp models-anlz/$tb/log.out $TGT/models-anlz/$tb/log-l1-a1.out
     elif [ $exp == "l1-a2" ]; then
         op_ep_anl=$(tail -1 models-anlz/$tb/log-l1a2.out | cut -f 1)
         input_anlz_model=models-anlz/$tb/anlz_fine-seq_"$op_ep_anl".pth
+        cp models-anlz/$tb/log-l1a2.out $TGT/models-anlz/$tb/log-l1-a2.out
         # op_ep_anl=$(tail -1 models-anlz/$tb/log-$exp.out | cut -f 1)
         # input_anlz_model=models-anlz/$tb/"$exp"_"$op_ep_anl".pth
     else
         op_ep_anl=$(tail -1 models-anlz/$tb/log-$exp.out | cut -f 1)
         input_anlz_model=models-anlz/$tb/"$exp"_"$op_ep_anl".pth
+        cp models-anlz/$tb/log-$exp.out $TGT/models-anlz/$tb/log-$exp.out
     fi
 
     cp models-anlz/$tb/log.out $TGT/models-anlz/$tb/log.out
